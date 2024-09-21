7123b3d95d2ac27e5e88042f2dae1e0d8203c281
Michael Gorachev <124209312+GoosikSpace@users.noreply.github.com>
2024-09-21 14:01:44 +0400

    The product filter on the site page and urlencode

--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,28 @@
 from django.template import context
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from traitlets import default
 
 from goods.models import Products
 
 def catalog(request, category_slug):
+    
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
    
+    
     if category_slug == 'all':
         goods = Products.objects.all() 
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+    
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
          
