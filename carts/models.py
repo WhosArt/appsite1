c497408a4321ff54126c22d451280606382172ca
Michael Gorachev <124209312+GoosikSpace@users.noreply.github.com>
2024-10-04 16:19:22 +0400

    Admin panel setup

--- carts/models.py
+++ carts/models.py
@@ -35,4 +35,6 @@ class Cart(models.Model):
 
 
     def __str__(self):
+        if self.user:
+            return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
         return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
